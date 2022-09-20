da987f964e55cd0dbd095a9953ea45d898d1ed09
thecoachanna <nettles.anna@gmail.com>
2022-09-19 21:28:34 -0400

    changed CORS

--- app.js
+++ app.js
@@ -21,14 +21,22 @@ const userRoutes = require('./routes/userRoutes')
 
 
 // Middleware
-app.use(
-  cors({
-      // React app
-      origin: ["http://localhost:3000", "*"], 
-      methods: "GET,POST,PUT,DELETE",
-      credentials: true,
-    })
-  );
+const allowedOrigins = ['http://localhost:3000',
+                      '*'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(morgan('tiny'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
